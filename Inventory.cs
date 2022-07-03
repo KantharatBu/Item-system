37d78f70df57f9b04eda03ec4fe9f423c52d6c6c
KantharatBu <106552900+KantharatBu@users.noreply.github.com>
2022-07-03 14:51:34 +0700

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    public GameObject inventoryObject;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            inventoryObject.SetActive(!inventoryObject.activeInHierarchy);
+        }
+    }
+}
